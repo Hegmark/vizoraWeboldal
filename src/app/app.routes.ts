9adba42f2b81ca9385e20c6c82f6961c8bc22d25
Hegedűs Márk <hegemark08@gmail.com>
2024-05-04 17:07:55 +0200

    routing done, error and not found pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { InitialComponent } from './pages/initial/initial.component';
+import { ReaderLoginComponent } from './pages/reader-login/reader-login.component';
+import { ReaderRegistComponent } from './pages/reader-regist/reader-regist.component';
+import { CivilListComponent } from './pages/civil-list/civil-list.component';
+import { AddressRegisterComponent } from './pages/address-register/address-register.component';
+import { ReaderDataComponent } from './pages/reader-data/reader-data.component';
+import { CivilDataComponent } from './pages/civil-data/civil-data.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { ReaderListComponent } from './pages/reader-list/reader-list.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: InitialComponent },
+    { path: 'reader-login', component: ReaderLoginComponent },
+    { path: 'reader-register', component: ReaderRegistComponent },
+    { path: 'civil-list', component: CivilListComponent },
+    { path: 'address-register', component: AddressRegisterComponent },
+    { path: 'reader-data', component: ReaderDataComponent },
+    { path: 'civil-data', component: CivilDataComponent },
+    { path: 'error', component: ErrorComponent },
+    { path: 'reader-list', component: ReaderListComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: '/not-found' },
+];
--- src/app/pages/initial/initial.component.ts
+++ src/app/pages/initial/initial.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-initial',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './initial.component.html',
   styleUrl: './initial.component.css'
 })
 export class InitialComponent {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  civilListNavigate() {
+    this.router.navigate(['civil-list']);
+  }
+  readerLoginNavigate(){
+    this.router.navigate(['reader-login']);
+  }
+
 }
